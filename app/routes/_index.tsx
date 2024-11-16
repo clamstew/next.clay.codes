4dde81639149263810f624e717f9b2f20e34974d
Clay Stewart <claytonmstewart@gmail.com>
2024-11-16 16:21:28 -0600

    update site title and description

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,8 +2,11 @@ import type { MetaFunction } from "@netlify/remix-runtime";
 import App from "../components/App";
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "clay.codes" },
+    {
+      name: "description",
+      content: "Slowly building a computer online, I guess. 🤷‍♂️",
+    },
   ];
 };
 
