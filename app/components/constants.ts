9bfdaf109543168a2dc4cb7f7bd6e08ed9813367
Clay Stewart <claytonmstewart@gmail.com>
2024-11-24 17:08:44 -0600

    add more sites

--- app/components/constants.ts
+++ app/components/constants.ts
@@ -1,11 +1,13 @@
 export const goSiteToCommands = {
   x: "https://x.com/Clay_Stewart",
+  bsky: "https://bsky.app/profile/clay.codes",
   github: "https://github.com/clamstew",
   "hire me": "https://www.linkedin.com/in/claystewart/",
   // "box it up": "https://www.mybox.es",
   // asdf: "https://github.com/asdf-vm/asdf",
   "site code": "https://github.com/clamstew/next.clay.codes",
   notes: "https://notes.build",
+  insulin: "https://fat-protein.netlify.app",
 };
 
 export const terminalCommands = {
