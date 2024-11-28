04d639b4e8d0f75ece6a720e2e37bf8d512fea0e
Clay Stewart <claytonmstewart@gmail.com>
2024-11-27 21:55:34 -0600

    more responsive title

--- app/sections/Home/components/Title.tsx
+++ app/sections/Home/components/Title.tsx
@@ -1,5 +1,5 @@
 export const Title = () => (
-  <code className="text-[40px] font-normal select-none text-[rgb(0, 255, 255)]">
+  <code className="text-[14px] [@media(min-width:185px)]:text-[18px] [@media(min-width:245px)]:text-[24px] [@media(min-width:400px)]:text-[32px] [@media(min-width:640px)]:text-[40px] font-normal select-none text-[rgb(0, 255, 255)]">
     &lt;clay.codes /&gt;
   </code>
 );
