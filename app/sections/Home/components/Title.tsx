547381206e235b7b85fe74d1a8d505d1c3e43955
Clay Stewart <claytonmstewart@gmail.com>
2024-11-30 00:50:34 -0600

    turn back time adn make the title a love letter to monokai vs create-react-app 😛

--- app/sections/Home/components/Title.tsx
+++ app/sections/Home/components/Title.tsx
@@ -1,5 +1,9 @@
 export const Title = () => (
-  <code className="text-[14px] [@media(min-width:185px)]:text-[18px] [@media(min-width:245px)]:text-[24px] [@media(min-width:400px)]:text-[32px] [@media(min-width:640px)]:text-[40px] font-normal select-none text-[rgb(0, 255, 255)]">
-    &lt;clay.codes /&gt;
+  <code className="text-[14px] [@media(min-width:185px)]:text-[18px] [@media(min-width:245px)]:text-[24px] [@media(min-width:400px)]:text-[32px] [@media(min-width:640px)]:text-[40px] font-normal select-none">
+    <span className="text-[#F92672]">&lt;</span>
+    <span className="text-[#A6E22E]">clay</span>
+    <span className="text-[#FD971F]">.codes</span>
+    <span className="text-[#66D9EF]"> /</span>
+    <span className="text-[#F92672]">&gt;</span>
   </code>
 );
